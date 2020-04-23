57b0c5480f9a59b3f0c9743622e12b017fe379a8
hujuu <out.of.the.base.sfc@gmail.com>
2020-04-23 18:07:40 +0900

    Update epd_213in_ad.py

--- python/examples/epd_213in_ad.py
+++ python/examples/epd_213in_ad.py
@@ -36,15 +36,13 @@ try:
     HRYimage = Image.new('1', (epd.height, epd.width), 255)  # 298*126  ryimage: red or yellow image
 
     newimage = Image.open(os.path.join(picdir, '100x100.bmp'))
-    HBlackimage.paste(newimage, (10, 10))
+    HBlackimage.paste(newimage, (0, 0))
 
     drawblack = ImageDraw.Draw(HBlackimage)
     drawry = ImageDraw.Draw(HRYimage)
     drawblack.text((10, 0), 'hello world', font=font20, fill=0)
     drawblack.text((10, 20), '2.13inch e-Paper bc', font=font20, fill=0)
     drawblack.text((120, 0), u'微雪电子', font=font20, fill=0)
-    drawblack.line((20, 50, 70, 100), fill=0)
-    drawblack.line((70, 50, 20, 100), fill=0)
     drawry.line((165, 50, 165, 100), fill=0)
     drawry.line((140, 75, 190, 75), fill=0)
     drawry.arc((140, 50, 190, 100), 0, 360, fill=0)
